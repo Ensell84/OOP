dcdb5e14072372c40d66651a3b68ec6a596c1bc4
Ensell84 <Ensell84@users.noreply.github.com>
2024-09-10 20:27:57 +0000

    Deploying to gh-pages from @ Ensell84/OOP@6e56784d054543f2a2eb27036f4e256b73cf505a 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.bondar","l":"HeapSort"},{"p":"ru.nsu.bondar","l":"Main"},{"p":"ru.nsu.bondar","l":"TimeMeasurment"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.bondar","l":"HeapSort"},{"p":"ru.nsu.bondar","l":"Main"}]
