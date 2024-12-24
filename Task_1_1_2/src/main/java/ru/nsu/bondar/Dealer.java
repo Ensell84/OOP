bf5ef3bc6b38cbb5ebd9be8ac306db41620c6909
Egor Bondar <e.bondar1@g.nsu.ru>
2024-12-25 04:18:44 +0700

    Javadocs to Player and Dealer

--- Task_1_1_2/src/main/java/ru/nsu/bondar/Dealer.java
+++ Task_1_1_2/src/main/java/ru/nsu/bondar/Dealer.java
@@ -1,12 +1,23 @@
 package ru.nsu.bondar;
 
+/**
+ * Represents the dealer in the Blackjack game.
+ */
 public class Dealer {
     private final Hand hand;
 
+    /**
+     * Constructs a new Dealer with an empty hand.
+     */
     public Dealer() {
         hand = new Hand();
     }
 
+    /**
+     * Returns the hand of the dealer.
+     *
+     * @return the hand of the dealer
+     */
     public Hand getHand() {
         return hand;
     }
--- Task_1_1_2/src/main/java/ru/nsu/bondar/Player.java
+++ Task_1_1_2/src/main/java/ru/nsu/bondar/Player.java
@@ -1,12 +1,23 @@
 package ru.nsu.bondar;
 
+/**
+ * Represents a player in the Blackjack game.
+ */
 public class Player {
     private final Hand hand;
 
+    /**
+     * Constructs a new Player with an empty hand.
+     */
     public Player() {
         hand = new Hand();
     }
 
+    /**
+     * Returns the hand of the player.
+     *
+     * @return the hand of the player
+     */
     public Hand getHand() {
         return hand;
     }
