afc208b5b11178a2935f8c25338684a5900d4ca2
Egor Bondar <a23134522@icloud.com>
2025-06-28 16:53:55 +0700

    Tests for DSL parsing subsystem

--- Task_2_4_1/src/main/java/ru/nsu/bondar/config/ConfigParser.groovy
+++ Task_2_4_1/src/main/java/ru/nsu/bondar/config/ConfigParser.groovy
@@ -1,7 +1,5 @@
 package ru.nsu.bondar.config
 
-import groovy.lang.Binding
-import groovy.lang.GroovyShell
 import ru.nsu.bondar.dsl.ConfigBuilder
 
 class ConfigParser {
@@ -16,7 +14,7 @@ class ConfigParser {
         binding.setVariable('tasks', { closure -> configBuilder.tasks(closure) })
         binding.setVariable('groups', { closure -> configBuilder.groups(closure) })
         binding.setVariable('checkpoints', { closure -> configBuilder.checkpoints(closure) })
-        
+
         def shell = new GroovyShell(binding)
         shell.evaluate(configFile)
 
